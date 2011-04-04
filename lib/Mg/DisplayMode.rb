30b798aa073547ba4ed0ddc43394a5742a452540
Matheus Moreira <matheus.a.m.moreira@gmail.com>
2011-04-04 16:14:23 -0300

    Added missing parent module, aliases and to_s to DisplayMode.

--- lib/Mg/DisplayMode.rb
+++ lib/Mg/DisplayMode.rb
@@ -1,9 +1,18 @@
-class DisplayMode
+module Mg
+  class DisplayMode
 
-  attr :width, :height, :bits_per_pixel
-  alias :bpp :bits_per_pixel
+    attr :width, :height, :bits_per_pixel
 
-  def initialize w, h, bpp
-    @width, @height, @bits_per_pixel = w, h, bpp
+    alias :w :width
+    alias :h :height
+    alias :bpp :bits_per_pixel
+
+    def initialize w, h, bpp
+      @width, @height, @bits_per_pixel = w, h, bpp
+    end
+
+    def to_s
+      "#{width}x#{height}@#{bits_per_pixel}"
+    end
   end
 end
