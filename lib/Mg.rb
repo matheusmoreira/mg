efcf4d3c2cfb2cb723458a269c9f57d66bf29b90
Matheus Moreira <matheus.a.m.moreira@gmail.com>
2011-04-04 04:10:24 -0300

    Added DisplayMode.rb require to Mg.rb.

--- lib/Mg.rb
+++ lib/Mg.rb
@@ -1,1 +1,2 @@
+require_relative 'Mg/DisplayMode'
 require 'Mn'
--- lib/Mg/DisplayMode.rb
+++ lib/Mg/DisplayMode.rb
@@ -1,7 +1,7 @@
 class DisplayMode
 
   attr :width, :height, :bits_per_pixel
-  alias :bpp, :bits_per_pixel
+  alias :bpp :bits_per_pixel
 
   def initialize w, h, bpp
     @width, @height, @bits_per_pixel = w, h, bpp
