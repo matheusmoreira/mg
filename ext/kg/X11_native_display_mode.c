2719698ef93f25069c7fca969802f1873458997c
Matheus Moreira <matheus.a.m.moreira@gmail.com>
2011-10-07 15:11:36 -0300

    Describing the implementation of the X11 display mode and adding a few helper methods.

--- ext/kg/X11_native_display_mode.c
+++ ext/kg/X11_native_display_mode.c
@@ -11,31 +11,59 @@
  * Returns the current display mode.
  */
 VALUE mg_native_display_mode_get_current_mode(VALUE klass) {
-    Display * d = XOpenDisplay(0);
-    if (d == 0) {
-        rb_raise(rb_eRuntimeError, "could not open Display");
-    }
-    int check, s = XDefaultScreen(d);
-    if (!XQueryExtension(d, "RANDR", &check, &check, &check)) {
-        rb_raise(rb_eRuntimeError, "Xrandr is not present - cannot get supported video modes");
-    }
-    XRRScreenConfiguration * config = XRRGetScreenInfo(d, RootWindow(d, s));
-    if (config == 0) {
-        rb_raise(rb_eRuntimeError, "could not retrieve screen configuration");
-    }
-    Rotation currentRotation;
-    int currentMode = XRRConfigCurrentConfiguration(config, &currentRotation);
-    int numSizes = 0;
-    XRRScreenSize * sizes = XRRConfigSizes(config, &numSizes);
     VALUE mode = Qnil;
+    Display * d = 0;
+    XRRScreenConfiguration * config = 0;
+    XRRScreenSize * sizes = 0;
+    int * depths = 0;
+    Rotation currentRotation;
+    int s, numSizes = 0, currentMode = 0;
+
+    /* Open connection to the X Display Server */
+    d = XOpenDisplay(0);
+
+    /* Raise if display is invalid */
+    check_display(d);
+
+    /* Raise if Xrandr is not present */
+    look_for_Xrandr(d);
+
+    /* Get the default screen identifier for this display */
+    s = XDefaultScreen(d);
+
+    /* Get screen information */
+    config = XRRGetScreenInfo(d, RootWindow(d, s));
+
+    /* Raise if the screen configuration is invalid */
+    check_screen_configuration(config);
+
+    /* Get current display configuration */
+    currentMode = XRRConfigCurrentConfiguration(config, &currentRotation);
+
+    /* Get all avaliable screen dimensions */
+    sizes = XRRConfigSizes(config, &numSizes);
+
+    /* If at least one screen size could be retrieved... */
     if (numSizes > 0) {
+
+        /*
+         * Create a new Mg::DisplayMode containing the width and height of the
+         * current display mode, and the value of the default depth for this
+         * X server and screen.
+         */
         mode = mg_display_mode_new(klass,
                                    sizes[currentMode].width,
                                    sizes[currentMode].height,
                                    DefaultDepth(d, s));
     }
+
+    /* Free the screen information structure */
     XRRFreeScreenConfigInfo(config);
+
+    /* Close the connection to the display server */
     XCloseDisplay(d);
+
+    /* return the current display mode */
     return mode;
 }
 
@@ -43,40 +71,101 @@ VALUE mg_native_display_mode_get_current_mode(VALUE klass) {
  * Returns all available modes in a Ruby array.
  */
 VALUE mg_native_display_mode_get_modes(VALUE klass) {
-    Display * d = XOpenDisplay(0);
-    if (d == 0) {
-        rb_raise(rb_eRuntimeError, "could not open Display");
-    }
-    int check, s = XDefaultScreen(d);
-    if (!XQueryExtension(d, "RANDR", &check, &check, &check)) {
-        rb_raise(rb_eRuntimeError, "Xrandr is not present - cannot get supported video modes");
-    }
-    XRRScreenConfiguration * config = XRRGetScreenInfo(d, RootWindow(d, s));
-    if (config == 0) {
-        rb_raise(rb_eRuntimeError, "could not retrieve screen configuration");
-    }
-    VALUE modes = rb_ary_new();
-    int numSizes = 0;
-    XRRScreenSize * sizes = XRRConfigSizes(config, &numSizes);
+    VALUE mode = Qnil, modes = Qnil;
+    Display * d = 0;
+    XRRScreenConfiguration * config = 0;
+    XRRScreenSize * sizes = 0;
+    int * depths = 0;
+    int s, i, j, numSizes = 0, numDepths = 0;
+
+    /* Open connection to the X Display Server */
+    d = XOpenDisplay(0);
+
+    /* Raise if display is invalid */
+    check_display(d);
+
+    /* Raise if Xrandr is not present */
+    look_for_Xrandr(d);
+
+    /* Get the default screen identifier for this display */
+    s = XDefaultScreen(d);
+
+    /* Get screen configuration information */
+    config = XRRGetScreenInfo(d, RootWindow(d, s));
+
+    /* Raise if the screen configuration is invalid */
+    check_screen_configuration(config);
+
+    /* Retrieve screen size */
+    numSizes = 0;
+    sizes = XRRConfigSizes(config, &numSizes);
+
+    /* If at least one screen configuration could be retrieved... */
     if (numSizes > 0) {
-        int numDepths = 0;
-        int * depths = XListDepths(d, s, &numDepths);
+
+        /* Retrieve screen depths. */
+        numDepths = 0;
+        depths = XListDepths(d, s, &numDepths);
+
+        /* If at least one depth could be retrieved... */
         if (numDepths > 0) {
-            int i;
+
+            /* Create new Ruby array in order to store the display modes */
+            modes = rb_ary_new();
+
+            /* For every depth at index i... */
             for (i = 0; i < numDepths; ++i) {
-                int j;
+
+                /* And for every screen at index j... */
                 for (j = 0; j < numSizes; ++j) {
-                    VALUE mode = mg_display_mode_new(klass,
-                                                     sizes[j].width,
-                                                     sizes[j].height,
-                                                     depths[i]);
+
+                    /*
+                     * Create a new Mg::DisplayMode containing the width and
+                     * height of the current screen, and the value of the
+                     * current depth
+                     */
+                    mode = mg_display_mode_new(klass,
+                                               sizes[j].width,
+                                               sizes[j].height,
+                                               depths[i]);
+
+                    /* Then add it to the ruby array */
                     rb_ary_push(modes, mode);
                 }
             }
         }
+
+        /* Free the array of depths */
         XFree(depths);
     }
+
+    /* Free the screen information structure */
     XRRFreeScreenConfigInfo(config);
+
+    /* Close the connection to the display server */
     XCloseDisplay(d);
+
+    /* Return the display modes */
     return modes;
+}
+
+void check_display(Display * d) {
+    if (d == 0) {
+        rb_raise(rb_eRuntimeError, "could not open Display");
+    }
+}
+
+void look_for_Xrandr(Display * d) {
+    int check;
+    if (!XQueryExtension(d, "RANDR", &check, &check, &check)) {
+        rb_raise(rb_eRuntimeError, "Xrandr is not present");
+        return Qnil;
+    }
+}
+
+void check_screen_configuration(XRRScreenConfiguration * config) {
+    if (config == 0) {
+        rb_raise(rb_eRuntimeError, "could not retrieve screen configuration");
+        return Qnil;
+    }
 }
