fdfc4c05fc1281692191b300ba9a2da52709647a
Matheus Moreira <matheus.a.m.moreira@gmail.com>
2011-10-07 16:29:24 -0300

    DRYing up X11 Display Mode implementation.

--- ext/kg/X11_native_display_mode.c
+++ ext/kg/X11_native_display_mode.c
@@ -7,41 +7,136 @@
 #include <X11/Xlib.h>
 #include <X11/extensions/Xrandr.h>
 
+/* DisplayData structure definition */
+
+struct DisplayData {
+    Display * display;
+    int screen;
+    XRRScreenConfiguration * screen_configuration;
+};
+
+/* Function prototypes */
+
+/**
+ * Opens a connection to the X Server, obtains the default screen and
+ * information about its configuration. Stores all this data in an instance of
+ * the DisplayData structure and then invokes the function pointer, passing it
+ * as the parameter.
+ *
+ * This function is responsible for freeing the screen configuration information
+ * and closing the connection to the X Server.
+ */
+static VALUE with_current_screen_configuration(VALUE (*f)(VALUE, struct DisplayData *), VALUE);
+
+/**
+ * Finds the current screen size and depth, given the DisplayData, and returns a
+ * Mg::DisplayMode Ruby object containing the information for the current
+ * display mode.
+ */
+static VALUE find_current_display_mode(VALUE, struct DisplayData *);
+
+/**
+ * Finds all available screen sizes and depths, given the DisplayData, and
+ * returns a Ruby array containing Mg::DisplayMode ruby objects, one for each
+ * available display configuration.
+ */
+static VALUE find_all_display_modes(VALUE, struct DisplayData *);
+
+/* Native interface implementation */
+
+/**
+ * Returns all available display modes in a Ruby array.
+ */
+VALUE mg_native_display_mode_get_modes(VALUE klass) {
+    return with_current_screen_configuration(&find_all_display_modes, klass);
+}
+
 /**
  * Returns the current display mode.
  */
 VALUE mg_native_display_mode_get_current_mode(VALUE klass) {
-    VALUE mode = Qnil;
-    Display * d = 0;
-    XRRScreenConfiguration * config = 0;
-    XRRScreenSize * sizes = 0;
-    int * depths = 0;
-    Rotation currentRotation;
-    int s, numSizes = 0, currentMode = 0;
+    return with_current_screen_configuration(&find_current_display_mode, klass);
+}
+
+/* Helper function prototypes */
+
+/**
+ * Allocates memory for a new instance of the DisplayData structure and returns
+ * a pointer to it.
+ */
+struct DisplayData * DisplayData_new();
+
+/**
+ * Checks if the display passed as parameter is valid. Raises a Ruby exception
+ * if it isn't.
+ */
+void check_display(Display *);
+
+/**
+ * Checks if the Xrandr extension is present on the X Server bound to the
+ * connection to the display passed as parameter. Raises a Ruby exception if the
+ * extension isn't present.
+ */
+void look_for_Xrandr(Display *);
+
+/**
+ * Checks if the screen configuration passed as parameter is valid. Raises a
+ * Ruby exception if it isn't.
+ */
+void check_screen_configuration(XRRScreenConfiguration * config);
+
+/* Helper function implementation */
+
+static VALUE with_current_screen_configuration(VALUE (*f)(VALUE, struct DisplayData *), VALUE klass) {
+    VALUE value = Qnil;
+    struct DisplayData * data = DisplayData_new();
 
     /* Open connection to the X Display Server */
-    d = XOpenDisplay(0);
-
-    /* Raise if display is invalid */
-    check_display(d);
+    data->display = XOpenDisplay(0);
+    check_display(data->display);
 
     /* Raise if Xrandr is not present */
-    look_for_Xrandr(d);
+    look_for_Xrandr(data->display);
 
     /* Get the default screen identifier for this display */
-    s = XDefaultScreen(d);
+    data->screen = XDefaultScreen(data->display);
 
     /* Get screen information */
-    config = XRRGetScreenInfo(d, RootWindow(d, s));
+    data->screen_configuration = XRRGetScreenInfo(data->display,
+                                                  RootWindow(data->display,
+                                                             data->screen));
 
     /* Raise if the screen configuration is invalid */
-    check_screen_configuration(config);
+    check_screen_configuration(data->screen_configuration);
+
+    /* Call the function and store its result */
+    value = f(klass, data);
+
+    /* Free the screen information structure */
+    XRRFreeScreenConfigInfo(data->screen_configuration);
+
+    /* Close the connection to the display server */
+    XCloseDisplay(data->display);
+
+    /* Free display data */
+    free(data);
+
+    /* Return the result of the function */
+    return value;
+}
+
+static VALUE find_current_display_mode(VALUE klass, struct DisplayData * data) {
+    VALUE mode = Qnil;
+    XRRScreenSize * sizes = 0;
+    Rotation currentRotation;
+    int numSizes = 0, currentMode = 0;
 
     /* Get current display configuration */
-    currentMode = XRRConfigCurrentConfiguration(config, &currentRotation);
+    currentMode = XRRConfigCurrentConfiguration(data->screen_configuration,
+                                                &currentRotation);
 
     /* Get all avaliable screen dimensions */
-    sizes = XRRConfigSizes(config, &numSizes);
+    sizes = XRRConfigSizes(data->screen_configuration, &numSizes);
 
     /* If at least one screen size could be retrieved... */
     if (numSizes > 0) {
@@ -54,58 +149,29 @@ VALUE mg_native_display_mode_get_current_mode(VALUE klass) {
         mode = mg_display_mode_new(klass,
                                    sizes[currentMode].width,
                                    sizes[currentMode].height,
-                                   DefaultDepth(d, s));
+                                   DefaultDepth(data->display, data->screen));
     }
-
-    /* Free the screen information structure */
-    XRRFreeScreenConfigInfo(config);
-
-    /* Close the connection to the display server */
-    XCloseDisplay(d);
 
     /* return the current display mode */
     return mode;
 }
 
-/**
- * Returns all available modes in a Ruby array.
- */
-VALUE mg_native_display_mode_get_modes(VALUE klass) {
+static VALUE find_all_display_modes(VALUE klass, struct DisplayData * data) {
     VALUE mode = Qnil, modes = Qnil;
-    Display * d = 0;
-    XRRScreenConfiguration * config = 0;
     XRRScreenSize * sizes = 0;
     int * depths = 0;
-    int s, i, j, numSizes = 0, numDepths = 0;
-
-    /* Open connection to the X Display Server */
-    d = XOpenDisplay(0);
-
-    /* Raise if display is invalid */
-    check_display(d);
-
-    /* Raise if Xrandr is not present */
-    look_for_Xrandr(d);
-
-    /* Get the default screen identifier for this display */
-    s = XDefaultScreen(d);
-
-    /* Get screen configuration information */
-    config = XRRGetScreenInfo(d, RootWindow(d, s));
-
-    /* Raise if the screen configuration is invalid */
-    check_screen_configuration(config);
+    int i, j, numSizes = 0, numDepths = 0;
 
     /* Retrieve screen size */
     numSizes = 0;
-    sizes = XRRConfigSizes(config, &numSizes);
+    sizes = XRRConfigSizes(data->screen_configuration, &numSizes);
 
     /* If at least one screen configuration could be retrieved... */
     if (numSizes > 0) {
 
         /* Retrieve screen depths. */
         numDepths = 0;
-        depths = XListDepths(d, s, &numDepths);
+        depths = XListDepths(data->display, data->screen, &numDepths);
 
         /* If at least one depth could be retrieved... */
         if (numDepths > 0) {
@@ -139,13 +205,7 @@ VALUE mg_native_display_mode_get_modes(VALUE klass) {
         XFree(depths);
     }
 
-    /* Free the screen information structure */
-    XRRFreeScreenConfigInfo(config);
-
-    /* Close the connection to the display server */
-    XCloseDisplay(d);
-
-    /* Return the display modes */
+    /* Return the available display modes */
     return modes;
 }
 
@@ -159,13 +219,15 @@ void look_for_Xrandr(Display * d) {
     int check;
     if (!XQueryExtension(d, "RANDR", &check, &check, &check)) {
         rb_raise(rb_eRuntimeError, "Xrandr is not present");
-        return Qnil;
     }
 }
 
 void check_screen_configuration(XRRScreenConfiguration * config) {
     if (config == 0) {
         rb_raise(rb_eRuntimeError, "could not retrieve screen configuration");
-        return Qnil;
     }
+}
+
+struct DisplayData * DisplayData_new() {
+    return malloc(sizeof(struct DisplayData));
 }
