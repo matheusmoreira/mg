a40c706cea5eeb21ca05aee8fbf75789c008c02c
Matheus Moreira <matheus.a.m.moreira@gmail.com>
2011-10-08 00:52:09 -0300

    Making helper method static.

--- ext/kg/X11_native_window.c
+++ ext/kg/X11_native_window.c
@@ -3,11 +3,7 @@
 
 #include <ruby.h>
 
-X11_Window * mg_window_from(VALUE obj) {
-    X11_Window * w;
-    Data_Get_Struct(obj, X11_Window, w);
-    return w;
-}
+static X11_Window * X11_Window_from(VALUE obj);
 
 VALUE mg_native_window_alloc(VALUE klass) {
     X11_Window * w = X11_Window_new();
@@ -19,52 +15,52 @@ void mg_native_window_init(VALUE self,
                            const char * name,
                            int x, int y,
                            unsigned int w, unsigned int h) {
-    X11_Window * window = mg_window_from(self);
+    X11_Window * window = X11_Window_from(self);
     X11_Window_init(window);
     X11_Window_create(window, x, y, w, h);
     X11_Window_set_name(window, name);
 }
 
 int mg_native_window_x(VALUE self) {
-    return X11_Window_get_attributes(mg_window_from(self)).x;
+    return X11_Window_get_attributes(X11_Window_from(self)).x;
 }
 
 int mg_native_window_y(VALUE self) {
-    return X11_Window_get_attributes(mg_window_from(self)).y;
+    return X11_Window_get_attributes(X11_Window_from(self)).y;
 }
 
 unsigned int mg_native_window_w(VALUE self) {
-    return X11_Window_get_attributes(mg_window_from(self)).width;
+    return X11_Window_get_attributes(X11_Window_from(self)).width;
 }
 
 unsigned int mg_native_window_h(VALUE self) {
-    return X11_Window_get_attributes(mg_window_from(self)).height;
+    return X11_Window_get_attributes(X11_Window_from(self)).height;
 }
 
 const char * mg_native_window_name(VALUE self) {
-    return X11_Window_name(mg_window_from(self));
+    return X11_Window_name(X11_Window_from(self));
 }
 
 int mg_native_window_visible(VALUE self) {
-    return X11_Window_visible(mg_window_from(self));
+    return X11_Window_visible(X11_Window_from(self));
 }
 
 void mg_native_window_set_pos(VALUE self, int x, int y) {
-    X11_Window_set_pos(mg_window_from(self), x, y);
+    X11_Window_set_pos(X11_Window_from(self), x, y);
 }
 
 void mg_native_window_set_size(VALUE self, unsigned int w, unsigned int h) {
-    X11_Window_set_size(mg_window_from(self), w, h);
+    X11_Window_set_size(X11_Window_from(self), w, h);
 }
 
 void mg_native_window_set_area(VALUE self,
                                int x, int y,
                                unsigned int w, unsigned int h) {
-    X11_Window_set_area(mg_window_from(self), x, y, w, h);
+    X11_Window_set_area(X11_Window_from(self), x, y, w, h);
 }
 
 void mg_native_window_set_visible(VALUE self, int visible) {
-    X11_Window_set_visible(mg_window_from(self), visible);
+    X11_Window_set_visible(X11_Window_from(self), visible);
 }
 
 void mg_native_window_set_x(VALUE self, int x) {
@@ -84,9 +80,15 @@ void mg_native_window_set_h(VALUE self, unsigned int h) {
 }
 
 void mg_native_window_set_name(VALUE self, const char * name) {
-    X11_Window_set_name(mg_window_from(self), name);
+    X11_Window_set_name(X11_Window_from(self), name);
 }
 
 void mg_native_window_set_fullscreen(VALUE self, int fullscreen) {
-    X11_Window_set_fullscreen(mg_window_from(self), fullscreen);
+    X11_Window_set_fullscreen(X11_Window_from(self), fullscreen);
+}
+
+static X11_Window * X11_Window_from(VALUE obj) {
+    X11_Window * w = 0;
+    Data_Get_Struct(obj, X11_Window, w);
+    return w;
 }
